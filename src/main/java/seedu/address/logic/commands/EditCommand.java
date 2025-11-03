88bac29a4e2e7fdd9c17821d8415c8048be7952d
Timothy <timothytiayz@gmail.com>
2025-11-03 22:47:29 +0800

    Fix long line in Edit Command for checkStyle

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -72,7 +72,8 @@ public class EditCommand extends Command {
         List<Person> lastShownList = model.getFilteredPersonList();
 
         if (index.getZeroBased() >= lastShownList.size()) {
-            logger.warning("EditCommand: invalid index " + index.getOneBased() + " for list size " + lastShownList.size());
+            logger.warning("EditCommand: invalid index " + index.getOneBased()
+                    + " for list size " + lastShownList.size());
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
